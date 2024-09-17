74f4c40f3e6ce3aa8ce1f0c0897039cc0baa4ecb
Assembly Consultoria <assemblyconsultoria@gmail.com>
2024-09-17 20:20:25 -0300

    Curso de Angular

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,19 +1,15 @@
 import { Component, DoCheck, OnDestroy } from '@angular/core';
+import { UserService } from '../../_services/user.service';
+import { User } from '../../_models/user';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements DoCheck {
- num: number = 1;
-
-  ngDoCheck(): void {
-      console.log('Uma mudança foi feita!');
+export class HomeComponent {
+  user: User | undefined;
+  constructor(private userService: UserService) {
+    this.user = userService.getUser();
   }
-
-  adiciona1() {
-    this.num++;
-  }
-
 }
--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { User } from '../../_models/user';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from '../../_services/user.service';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit{
   //users: String[] = ['Fabio', 'Maria', 'Carla', 'Roberto']
+  userSelecionado: User | undefined;
+  userForm: FormGroup = new FormGroup({});
+
+  constructor(private fb: FormBuilder, private userService: UserService) {}
+
+  ngOnInit(): void {
+      this.initializeForm();
+  }
+
+  initializeForm() {
+    this.userForm = this.fb.group({
+      nome: ['', [Validators.required, Validators.maxLength(250)]],
+      idade: ['', [Validators.required, Validators.min(12), Validators.max(100)]]
+    });
+  }
+
+  SubmitForm() {
+    if(this.userForm.valid) {
+      this.users.push(this.userForm.value);
+      this.userForm.reset();
+    }
+
+  }
 
   users: User[] = [
     {
@@ -27,4 +52,9 @@ export class UsersComponent {
       idade: 16
     }
   ];
+
+  infoUserSelecionado(user: User) {
+    this.userSelecionado = user;
+    this.userService.setUser(user);
+  }
 }
