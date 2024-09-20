aa5c63858552ee6e4e7f589214c77c07df4913d0
Assembly Consultoria <assemblyconsultoria@gmail.com>
2024-09-19 21:15:13 -0300

    Criado a interface UserGit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, DoCheck, OnDestroy, OnInit } from '@angular/core';
 import { UserService } from '../../_services/user.service';
 import { User } from '../../_models/user';
+import { UserGit } from '../../_models/userGit';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,7 @@ import { User } from '../../_models/user';
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit {
-
+  user: UserGit | undefined;
   constructor(private userService: UserService) {
 
   }
@@ -18,8 +19,8 @@ export class HomeComponent implements OnInit {
   }
 
   getGitUser() {
-    this.userService.getGitUser('facebook').subscribe((response: any) => {
-      console.log(response)
+    this.userService.getGitUser('facebook').subscribe((response: UserGit) => {
+      this.user = response;
     });
   }
 }
